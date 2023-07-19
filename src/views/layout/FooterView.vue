c060e132424defc91b5d3e890c78d65fbd20f089
nzrkodi <nzrkodi@gmail.com>
2023-07-19 15:47:54 +0800

    create tampilan user

---
+++ src/views/layout/FooterView.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="footer bg-dark text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>About Our Laundry</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nulla eu aliquet fringilla.
+                    </p>
+                </div>
+                <div class="col-md-3">
+                    <h4>Quick Links</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h4>Contact Info</h4>
+                    <ul class="list-unstyled">
+                        <li><i class="fas fa-map-marker-alt"></i> Address, City, Country</li>
+                        <li><i class="fas fa-envelope"></i> email@example.com</li>
+                        <li><i class="fas fa-phone"></i> +1234567890</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="text-center">
+                <p>&copy; 2023 Your Laundry. All rights reserved.</p>
+                <div class="social-icons mb-4">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<script setup>
+</script>
---
+++ src/views/pages/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="loading-screen">
+        <div class="loader"></div>
+    </div>
+    <Header />
+    <main>
+        <section class="container section section__height" id="about">
+            <div class="row mt-4">
+                <div class="container-fluid">
+                    <section>
+                        <div class="row">
+                            <div class="col-xl-12 col-md-12">
+                                <div class="cardd">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between p-md-1">
+                                            <div class="d-flex flex-row">
+                                                <div class="align-self-center">
+                                                    <i class="fas fa-pencil-alt text-info fa-2x me-4"></i>
+                                                </div>
+                                                <div class="ml-2">
+                                                    <h4 class="edit">Total Posts</h4>
+                                                    <p class="mb-0 edit">Monthly blog posts</p>
+                                                </div>
+                                            </div>
+                                            <div class="align-self-center">
+                                                <h2 class="h3 mb-0 edit">18,000</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-12 col-md-12">
+                                <div class="cardd">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between p-md-1">
+                                            <div class="d-flex flex-row">
+                                                <div class="align-self-center">
+                                                    <i class="fas fa-pencil-alt text-info fa-2x me-4"></i>
+                                                </div>
+                                                <div class="ml-2">
+                                                    <h4 class="edit">Total Posts</h4>
+                                                    <p class="mb-0 edit">Monthly blog posts</p>
+                                                </div>
+                                            </div>
+                                            <div class="align-self-center">
+                                                <h2 class="h3 mb-0 edit">18,000</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+    </main>
+    <Footer />
+</template>
+<script setup>
+import Header from "../layout/HeaderView.vue";
+import Footer from "../layout/FooterView.vue";
+</script>
